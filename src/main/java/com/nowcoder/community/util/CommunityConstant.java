1ad2be2cab5ef77c47bb6d4f77078288be5c9e71
zian <zianwang623@gmail.com>
2023-03-12 16:05:08 -0400

    logout:   . new entity - loginticket

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -8,4 +8,14 @@ public interface CommunityConstant {
     int ACTIVATION_SUCCESS = 0;
     int ACTIVATION_REPEAT = 1;
     int ACTIVATION_FAILURE = 2;
+
+    /**
+     * 默认登录超时时间，不选’记住我‘
+     */
+    int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
+
+    /**
+     * ‘记住我’登录有效时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 }
