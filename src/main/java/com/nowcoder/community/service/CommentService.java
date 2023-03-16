8a657898ced2e11f2523d4bbd13e44a7b52dc60d
zian <zianwang623@gmail.com>
2023-03-16 17:24:45 -0400

    discuss post:   . write new comment and replies

--- src/main/java/com/nowcoder/community/service/CommentService.java
+++ src/main/java/com/nowcoder/community/service/CommentService.java
@@ -2,16 +2,29 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.CommentMapper;
 import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit){
         return commentMapper.selectCommentByEntity(entityType, entityId, offset, limit);
@@ -21,4 +34,27 @@ public class CommentService {
         return commentMapper.selectCountByEntity(entityType, entityId);
     }
 
+    // 需要事务，两个DAO操作：插入评论（comment 对象），更新评论数量（discussPost 对象），需要一次成功，或者完全失败，保证数据一致性
+    // 两种实现方式：1）注解方式，整个函数是一个事务
+    //            2）编程方式，详细控制整个事务
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment){
+        if(comment.getContent() != null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        //添加评论
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+        // 更新评论数量
+
+        // 更新帖子的评论数量
+        if(comment.getEntityType() == ENTITY_TYPE_POST){
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+        return rows;
+    }
 }
+
+
