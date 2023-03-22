51ea556b2cffd3ff0083231d95b251011f256e8a
zian <zianwang623@gmail.com>
2023-03-22 18:31:19 -0400

    kafka:   . installation in pom   . introduction and test

---
+++ src/test/java/com/nowcoder/community/BlockingQueueTests.java
@@ -0,0 +1,61 @@
+package com.nowcoder.community;
+
+import ch.qos.logback.core.encoder.EchoEncoder;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueTests {
+    public static void main(String[] args) {
+        BlockingQueue queue = new ArrayBlockingQueue(10); // 队列容量
+        new Thread(new Producer(queue)).start();
+        new Thread(new Consumer(queue)).start();
+        new Thread(new Consumer(queue)).start();
+        new Thread(new Consumer(queue)).start();
+    }
+}
+
+class Producer implements Runnable{
+
+    private BlockingQueue<Integer> queue;
+
+    public Producer(BlockingQueue<Integer> queue){
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try{
+            for(int i = 0; i < 100; i++){
+                Thread.sleep(20);
+                queue.put(i); //队列满了就阻塞
+                System.out.println(Thread.currentThread().getName() + "生产" + queue.size());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Consumer implements Runnable{
+
+    private BlockingQueue<Integer> queue;
+
+    public Consumer(BlockingQueue<Integer> queue){
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try{
+            while(true){
+                Thread.sleep(new Random().nextInt(1000));
+                queue.take();
+                System.out.println(Thread.currentThread().getName() + "消费了" + queue.size());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
